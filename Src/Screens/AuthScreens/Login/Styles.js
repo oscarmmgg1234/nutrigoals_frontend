a157888e4694b78aee5112763d0861fb2feed714
Oscar Maldonado-Garcia <oscarmmgg1234@gmail.com>
2021-06-08 14:21:09 -0700

    commit before pull ios assets

--- Src/Screens/AuthScreens/Login/Styles.js
+++ Src/Screens/AuthScreens/Login/Styles.js
@@ -1,23 +1,23 @@
-import { StyleSheet, Dimensions } from 'react-native';
+import {StyleSheet, Dimensions} from 'react-native';
 import Colors from '../../../Styles/Colors';
 const widthScreen = Dimensions.get('window').width;
 const styles = StyleSheet.create({
   safeViewStyle: {
     backgroundColor: Colors.backgroundColor,
-    flex: 1
+    flex: 1,
   },
   mainContainer: {
     marginTop: 20,
     width: widthScreen / 1.1,
     alignSelf: 'center',
-    marginBottom: 20
+    marginBottom: 20,
   },
   headerText: {
     fontSize: 35,
     fontWeight: '700',
     color: Colors.White,
-    textAlign: "center",
-    marginTop: '10%'
+    textAlign: 'center',
+    marginTop: '10%',
   },
   emailMainContainer: {
     marginTop: '2%',
@@ -26,19 +26,19 @@ const styles = StyleSheet.create({
   },
   inputTextStyle: {
     fontSize: 20,
-    fontWeight: "400",
+    fontWeight: '400',
     marginTop: '20%',
     color: Colors.White,
     width: widthScreen / 1.25,
-    alignSelf: 'center'
+    alignSelf: 'center',
   },
   inputTextStyle1: {
     fontSize: 20,
-    fontWeight: "400",
+    fontWeight: '400',
     marginTop: 20,
     color: Colors.White,
     width: widthScreen / 1.25,
-    alignSelf: 'center'
+    alignSelf: 'center',
   },
   emailWrapper: {
     marginTop: 20,
@@ -56,14 +56,15 @@ const styles = StyleSheet.create({
     marginLeft: 20,
     flex: 1,
     fontSize: 18,
-    margin: 10,
-    color: Colors.White
+    margin: 6,
+    color: Colors.White,
   },
   checkImage: {
     width: 25,
     height: 25,
     marginRight: 10,
-    marginTop: 15
+    marginTop: 15,
+    borderRadius: 6,
   },
   buttonContainer: {
     width: widthScreen / 1.25,
@@ -73,18 +74,18 @@ const styles = StyleSheet.create({
     backgroundColor: Colors.buttonColor,
     marginTop: '10%',
     alignSelf: 'center',
-    borderRadius: 20
+    borderRadius: 20,
   },
   buttonText: {
     color: Colors.White,
     fontSize: 20,
-    fontWeight: '600'
+    fontWeight: '600',
   },
   bottomWrapper: {
     flexDirection: 'row',
     justifyContent: 'center',
     alignItems: 'center',
-    marginTop: 30
+    marginTop: 30,
   },
   googleWrapper: {
     width: widthScreen / 1.25,
@@ -94,18 +95,18 @@ const styles = StyleSheet.create({
     backgroundColor: Colors.texInputBackground,
     marginTop: '7%',
     alignSelf: 'center',
-    borderRadius: 20
+    borderRadius: 20,
   },
   alreadyAccountText: {
     fontSize: 14,
     fontWeight: '500',
-    textAlign: "center",
+    textAlign: 'center',
     color: Colors.White,
   },
   loginTextBottom: {
     fontSize: 16,
     fontWeight: '500',
-    textAlign: "center",
+    textAlign: 'center',
     color: Colors.White,
     marginLeft: 10,
   },
@@ -114,19 +115,19 @@ const styles = StyleSheet.create({
     borderBottomColor: Colors.White,
     width: widthScreen / 1.3,
     alignSelf: 'center',
-    marginTop: 20
+    marginTop: 20,
   },
   socialTextStyling: {
     fontSize: 16,
     fontWeight: '500',
-    textAlign: "center",
+    textAlign: 'center',
     color: Colors.White,
-    marginTop: 25
+    marginTop: 25,
   },
   googleTextBottom: {
     color: Colors.White,
     fontSize: 24,
-    fontWeight: '600'
-  }
+    fontWeight: '600',
+  },
 });
 export default styles;
