6279ce3996fa2c0781e370092a9e28797c944df5
Oscar Maldonado-Garcia <oscarmmgg1234@gmail.com>
2021-07-27 22:18:25 -0700

    editMacroModal rework

--- Src/Components/EditModal/editMainModal.js
+++ Src/Components/EditModal/editMainModal.js
@@ -1,180 +1,162 @@
-
 import React, {useState} from 'react';
 import {Modal, View, Text, TouchableOpacity, Image, Switch} from 'react-native';
 import Styles from '../../Screens/Home/Styles';
 import * as Constants from '../../Constants/index';
 import Images from '../../Styles/Images';
 import Colors from '../../Styles/Colors';
-import Icon from 'react-native-vector-icons/FontAwesome'
-
+import Icon from 'react-native-vector-icons/FontAwesome';
+import MacroModal from './macroModal';
 
 const EditMainModal = (props) => {
+  const [modalVisibility, setModalV] = useState({
+    macroM: false,
+    waterM: false,
+    weightM: false,
+    notiM: false,
+    calc: false,
+  });
 
-    const [modalVisibility, setModalV] = useState({macroM: false, waterM: false, weightM: false, notiM: false, calc: false})
+  function setModalVisibility(modalIndex){
+    switch (modalIndex){
+      case 0: {
+        setModalV({macroM: false})
+        break;
+      }
+      case 1:{
+        setModalV({waterM: false})
+        break;
+      }case 2:{
+        setModalV({weightM: false})
+        break;
+      }case 3:{
+        setModalV({notiM: false})
+        break;
+      }case 4:{
+        setModalV({calc: false})
+        break;
+      }
 
+    }
+  }
 
-    return (<Modal
-        animationType={'slide'}
-        transparent={true}
-        visible={props.modal}
-        onRequestClose={() => props.toggleModal()}>
-        <View style={Styles.ModalContainer}>
-          <View style={Styles.modalHeader}>
-            <View style={Styles.ModalHeaderView}>
-              <View style={{display: 'flex', flexDirection:'row'}}>
-                <Icon name={'cog'} color={'white'} size={37} style={{marginTop: 7, marginRight: 8}}/>
-              <Text style={Styles.homeText}>
-                {Constants.SETTINGS}
-              </Text>
-              
-              </View>
-              
-              <TouchableOpacity
-
-                onPress={() => props.toggleModal()}>
-                <Icon name={'close'} color={'white'} size={35} style={{marginTop: 4}} />
-              </TouchableOpacity>
+  return (
+    <Modal
+      animationType={'slide'}
+      transparent={true}
+      visible={props.modal}
+      onRequestClose={() => props.toggleModal()}>
+      <View style={Styles.ModalContainer}>
+        <View style={Styles.modalHeader}>
+          <View style={Styles.ModalHeaderView}>
+            <View style={{display: 'flex', flexDirection: 'row'}}>
+              <Icon
+                name={'cog'}
+                color={'white'}
+                size={37}
+                style={{marginTop: 7, marginRight: 8}}
+              />
+              <Text style={Styles.homeText}>{Constants.SETTINGS}</Text>
             </View>
+
+            <TouchableOpacity onPress={() => props.toggleModal()}>
+              <Icon
+                name={'close'}
+                color={'white'}
+                size={35}
+                style={{marginTop: 4}}
+              />
+            </TouchableOpacity>
           </View>
-
-          <TouchableOpacity
-            style={{marginTop: 16}}
-            onPress={() => setModalV({macroM: true})}>
-            <View style={Styles.listWrapper}>
-              <View style={{flexDirection: 'row'}}>
-                <Icon name={'bullseye'} color={'coral'} size={27} style={{marginTop: 3}}/>
-                <Text style={Styles.listText}>
-                  {'Edit Macronutrient Goals '}
-                </Text>
-              </View>
-              <Image
-                source={Images.chevron}
-                style={Styles.userImage}
-              />
-            </View>
-            <View style={Styles.seperator} />
-          </TouchableOpacity>
-          <TouchableOpacity onPress={()=>setModalV({waterM: true})}>
-            <View style={Styles.listWrapper}>
-              <View style={{flexDirection: 'row'}}>
-              <Icon name={'glass'} color={'aquamarine'} size={25} style={{marginTop: 3}}/>
-                <Text style={Styles.listText}>
-                  {'Edit Water Goals'}
-                </Text>
-              </View>
-              <Image
-                source={Images.chevron}
-                style={Styles.userImage}
-              />
-            </View>
-            <View style={Styles.seperator} />
-          </TouchableOpacity>
-          <TouchableOpacity onPress={()=>setModalV({weightM: true})}>
-            <View style={Styles.listWrapper}>
-              <View style={{flexDirection: 'row'}}>
-              <Icon name={'line-chart'} color={'white'} size={24} style={{marginTop: 3}}/>
-                <Text style={Styles.listText}>
-                  {'Edit Weight Goals'}
-                </Text>
-              </View>
-              <Image
-                source={Images.chevron}
-                style={Styles.userImage}
-              />
-            </View>
-            <View style={Styles.seperator} />
-          </TouchableOpacity>
-          <TouchableOpacity onPress={()=>setModalV({notiM: true})}> 
-            <View style={Styles.listWrapper}>
-              <View style={{flexDirection: 'row'}}>
-              <Icon name={'window-maximize'} color={'purple'} size={25} style={{marginTop: 3}}/>
-                <Text style={Styles.listText}>
-                  {'Manage notifications'}
-                </Text>
-              </View>
-              <Image
-                source={Images.chevron}
-                style={Styles.userImage}
-              />
-            </View>
-            <View style={Styles.seperator} />
-          </TouchableOpacity>
-          <TouchableOpacity onPress={()=>setModalV({calc: true})}>
-            <View style={Styles.listWrapper}>
-              <View style={{flexDirection: 'row'}}>
-              <Icon name={'gears'} color={'violet'} size={25} style={{marginTop: 3}}/>
-                <Text style={Styles.listText}>
-                  {'Fitness calculators'}
-                </Text>
-              </View>
-              <Image
-                source={Images.chevron}
-                style={Styles.userImage}
-              />
-            </View>
-            <View style={Styles.seperator} />
-          </TouchableOpacity>
-          <TouchableOpacity
-            style={Styles.buttonContainer}
-            onPress={() => {
-              this.signOuty();
-            }}>
-            <Text style={Styles.buttonText}>{Constants.SIGNOUT}</Text>
-          </TouchableOpacity>
-          <Modal
-            animationType={'slide'}
-            transparent={true}
-            visible={modalVisibility.macroM}>
-            <View style={Styles.MacroModalContainer}>
-              <View style={Styles.modalHeader}>
-                <View style={Styles.ModalHeaderView}>
-                  <Text style={Styles.homeText}>
-                    {Constants.MACRONUTRIENT}
-                  </Text>
-                 
-                  <TouchableOpacity onPress={()=>setModalV({macroM: false})}>
-                    <Icon name={'close'} color={'white'} size={35} style={{marginTop: 4}} />
-                  </TouchableOpacity>
-                </View>
-              </View>
-              <View style={Styles.macroBodySwitchHeaderSwitch}>
-                <Text style={Styles.macroTextSwitch}>
-                  Select Method
-                </Text>
-              </View>
-              <View style={Styles.switchContainer}>
-                <Text
-                  style={
-                    modalVisibility.macroM === false
-                      ? {color: Colors.buttonColor}
-                      : {color: 'white'}
-                  }>
-                  {'<= Custom'}
-                </Text>
-                <Switch
-                  style={{transform: [{scaleX: 1.4}, {scaleY: 1.4}]}}
-                  trackColor={'coral'}
-                  thumbColor={Colors.backgroundColor}
-                  onValueChange={props.toggleMacro}
-                  value={true}
-                />
-                <Text
-                  style={
-                    modalVisibility.macroM === true
-                      ? {color: Colors.buttonColor}
-                      : {color: 'white'}
-                  }>
-                  {'Ratio =>'}
-                </Text>
-              </View>
-
-              <TouchableOpacity onPress={() => {}}>
-                <Text style={Styles.homeText}>Edit Macro</Text>
-              </TouchableOpacity>
-            </View>
-          </Modal>
         </View>
-      </Modal>)
-}
+
+        <TouchableOpacity
+          style={{marginTop: 16}}
+          onPress={() => setModalV({macroM: true})}>
+          <View style={Styles.listWrapper}>
+            <View style={{flexDirection: 'row'}}>
+              <Icon
+                name={'bullseye'}
+                color={'coral'}
+                size={27}
+                style={{marginTop: 3}}
+              />
+              <Text style={Styles.listText}>{'Edit Macronutrient Goals '}</Text>
+            </View>
+            <Image source={Images.chevron} style={Styles.userImage} />
+          </View>
+          <View style={Styles.seperator} />
+        </TouchableOpacity>
+        <TouchableOpacity onPress={() => setModalV({waterM: true})}>
+          <View style={Styles.listWrapper}>
+            <View style={{flexDirection: 'row'}}>
+              <Icon
+                name={'glass'}
+                color={'aquamarine'}
+                size={25}
+                style={{marginTop: 3}}
+              />
+              <Text style={Styles.listText}>{'Edit Water Goals'}</Text>
+            </View>
+            <Image source={Images.chevron} style={Styles.userImage} />
+          </View>
+          <View style={Styles.seperator} />
+        </TouchableOpacity>
+        <TouchableOpacity onPress={() => setModalV({weightM: true})}>
+          <View style={Styles.listWrapper}>
+            <View style={{flexDirection: 'row'}}>
+              <Icon
+                name={'line-chart'}
+                color={'white'}
+                size={24}
+                style={{marginTop: 3}}
+              />
+              <Text style={Styles.listText}>{'Edit Weight Goals'}</Text>
+            </View>
+            <Image source={Images.chevron} style={Styles.userImage} />
+          </View>
+          <View style={Styles.seperator} />
+        </TouchableOpacity>
+        <TouchableOpacity onPress={() => setModalV({notiM: true})}>
+          <View style={Styles.listWrapper}>
+            <View style={{flexDirection: 'row'}}>
+              <Icon
+                name={'window-maximize'}
+                color={'purple'}
+                size={25}
+                style={{marginTop: 3}}
+              />
+              <Text style={Styles.listText}>{'Manage notifications'}</Text>
+            </View>
+            <Image source={Images.chevron} style={Styles.userImage} />
+          </View>
+          <View style={Styles.seperator} />
+        </TouchableOpacity>
+        <TouchableOpacity onPress={() => setModalV({calc: true})}>
+          <View style={Styles.listWrapper}>
+            <View style={{flexDirection: 'row'}}>
+              <Icon
+                name={'gears'}
+                color={'violet'}
+                size={25}
+                style={{marginTop: 3}}
+              />
+              <Text style={Styles.listText}>{'Fitness calculators'}</Text>
+            </View>
+            <Image source={Images.chevron} style={Styles.userImage} />
+          </View>
+          <View style={Styles.seperator} />
+        </TouchableOpacity>
+        <TouchableOpacity
+          style={Styles.buttonContainer}
+          onPress={() => {
+            this.signOuty();
+          }}>
+          <Text style={Styles.buttonText}>{Constants.SIGNOUT}</Text>
+        </TouchableOpacity>
+        <MacroModal visibility={modalVisibility.macroM} setModalV={setModalVisibility}/>
+      </View>
+    </Modal>
+  );
+};
 
 export default EditMainModal;
---
+++ Src/Components/EditModal/macroModal.js
@@ -0,0 +1,120 @@
+import React, {useState} from 'react';
+import {
+  Modal,
+  View,
+  Text,
+  Switch,
+  TouchableOpacity,
+  ScrollView,
+} from 'react-native';
+import Icon from 'react-native-vector-icons/FontAwesome';
+import Styles from '../../Screens/Home/Styles';
+import Colors from '../../Styles/Colors';
+import * as Constants from '../../Constants';
+
+const MacroModal = (props) => {
+  const [switchBool, setSwitch] = useState(false);
+  return (
+    <Modal animationType={'fade'} transparent={true} visible={props.visibility}>
+      <View style={Styles.MacroModalContainer}>
+        <View style={Styles.modalHeader}>
+          <View style={Styles.ModalHeaderView}>
+            <Text style={Styles.homeText}>{Constants.MACRONUTRIENT}</Text>
+
+            <TouchableOpacity onPress={() => props.setModalV(0)}>
+              <Icon
+                name={'close'}
+                color={'white'}
+                size={35}
+                style={{marginTop: 4}}
+              />
+            </TouchableOpacity>
+          </View>
+        </View>
+        <View
+          style={{
+            height: '20%',
+            backgroundColor: 'rgba(40,40,50,0.9)',
+            borderBottomWidth: 1,
+            borderColor: Colors.backgroundColor,
+          }}>
+          <View style={Styles.macroBodySwitchHeaderSwitch}>
+            <Text
+              style={[
+                Styles.macroTextSwitch,
+                {fontSize: 20, fontWeight: 'bold'},
+              ]}>
+              {'Method:'}
+            </Text>
+          </View>
+          <View style={[Styles.switchContainer, {marginTop: 20}]}>
+            <View
+              style={{
+                display: 'flex',
+                flexDirection: 'row',
+                alignItems: 'center',
+              }}>
+              {switchBool != true ? (
+                <Icon
+                  name={'pencil'}
+                  color={switchBool != true ? Colors.buttonColor : 'white'}
+                  size={25}
+                  style={{marginRight: 10}}
+                />
+              ) : null}
+              <Text
+                style={
+                  switchBool != true
+                    ? {color: Colors.buttonColor, fontSize: 18}
+                    : {color: 'white', fontSize: 18}
+                }>
+                {'Custom'}
+              </Text>
+            </View>
+            <Switch
+              style={{transform: [{scaleX: 1.9}, {scaleY: 1.7}]}}
+              trackColor={'coral'}
+              thumbColor={Colors.grey}
+              onValueChange={() => setSwitch(!switchBool)}
+              value={switchBool}
+            />
+            <View
+              style={{
+                display: 'flex',
+                flexDirection: 'row',
+                alignItems: 'center',
+              }}>
+              <Text
+                style={
+                  switchBool === true
+                    ? {color: Colors.buttonColor, fontSize: 18}
+                    : {color: 'white', fontSize: 18}
+                }>
+                {'Ratio'}
+              </Text>
+              {switchBool === true ? (
+                <Icon
+                  name={'exchange'}
+                  color={switchBool === true ? Colors.buttonColor : 'white'}
+                  size={25}
+                  style={{marginLeft: 10}}
+                />
+              ) : null}
+            </View>
+          </View>
+        </View>
+        {switchBool === true ? (
+          <ScrollView>
+            <Text>Ratio</Text>
+          </ScrollView>
+        ) : (
+          <ScrollView>
+            <Text>Custom</Text>
+          </ScrollView>
+        )}
+      </View>
+    </Modal>
+  );
+};
+
+export default MacroModal;
