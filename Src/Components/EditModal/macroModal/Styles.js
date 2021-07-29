02ce92f96ae2979787be99cca761e234f443bf75
Oscar Maldonado-Garcia <oscarmmgg1234@gmail.com>
2021-07-29 14:37:04 -0700

    MacroCustomWork

--- Src/Components/EditModal/macroModal/Styles.js
+++ Src/Components/EditModal/macroModal/Styles.js
@@ -1,10 +1,10 @@
 import {StyleSheet} from 'react-native';
+import Colors from '../../../Styles/Colors';
 
 const Styles = StyleSheet.create({
   customContainer: {
     width: '100%',
     height: '100%',
-
   },
   InputView: {
     display: 'flex',
@@ -14,12 +14,38 @@ const Styles = StyleSheet.create({
     alignItems: 'center',
     marginTop: 30,
   },
-  InputView1:{
+  InputView1: {
+    marginTop: 20,
     display: 'flex',
     flexDirection: 'row',
     justifyContent: 'center',
     width: '100%',
     alignItems: 'center',
-  }
+  },
+  InputText: {
+    fontSize: 18,
+    fontWeight: 'bold',
+    color: 'white',
+  },
+  InputStyles: {
+    width: '40%',
+    backgroundColor: 'rgba(10,10,10,0.4)',
+    borderRadius: 20,
+    paddingLeft: 10,
+    color: 'white',
+    fontSize: 18,
+    paddingRight: 10,
+  },
+  InputSubview: {
+    paddingLeft: 8,
+    borderRadius: 20,
+    width: '50%',
+    backgroundColor: Colors.backgroundColor,
+    display: 'flex',
+    flexDirection: 'row',
+    height: 50,
+    alignItems: 'center',
+    justifyContent: 'center'
+  },
 });
 export default Styles;
--- Src/Components/EditModal/macroModal/macroCustom.js
+++ Src/Components/EditModal/macroModal/macroCustom.js
@@ -1,21 +1,48 @@
 import React, {useState} from 'react';
-import { Text, View, TextInput, ScrollView, TouchableOpacity } from "react-native";
+import {
+  Text,
+  View,
+  TextInput,
+  ScrollView,
+  TouchableOpacity,
+} from 'react-native';
 import Icon from 'react-native-vector-icons/FontAwesome';
 import Colors from '../../../Styles/Colors';
 import Styles from './Styles';
 
 const CustomForm = (props) => {
-  const [MacroInput, setMacroI] = useState('');
+  const [PI, setPI] = useState('');
+  const [FI, setFI] = useState('');
+  const [CI, setCI] = useState('');
+  const [SI, setSI] = useState('');
+  const [SOI, setSOI] = useState('');
 
   return (
     <ScrollView scrollEnabled={true}>
       <View style={Styles.customContainer}>
-        <Text style={{alignSelf: 'center', color: 'white', fontSize: 17, fontWeight: 'bold', marginTop: 20}}>{'Enter Macronutrients Goals Manually: '}</Text>
+        <Text
+          style={{
+            alignSelf: 'center',
+            color: 'white',
+            fontSize: 19,
+            marginTop: 20,
+          }}>
+          {'Enter Macronutrients Goals Manually:'}
+        </Text>
+        <View
+          style={{
+            height: 1,
+            width: '67%',
+            backgroundColor: Colors.backgroundColor,
+            alignSelf: 'center',
+            marginTop: 8,
+          }}
+        />
         <View style={Styles.InputView}>
-          <Text>{'Protein: '}</Text>
+          <Text style={Styles.InputText}>{'Protein: '}</Text>
           <TextInput
-            value={MacroInput}
-            onChangeText={setMacroI}
+            value={PI}
+            onChangeText={setPI}
             placeholder={'Enter Protein Goal'}
             placeholderTextColor={'rgba(255,255,255,0.6)'}
             style={Styles.InputStyles}
@@ -23,10 +50,10 @@ const CustomForm = (props) => {
           />
         </View>
         <View style={Styles.InputView1}>
-          <Text>{'Protein: '}</Text>
+          <Text style={Styles.InputText}>{'Protein: '}</Text>
           <TextInput
-            value={MacroInput}
-            onChangeText={setMacroI}
+            value={FI}
+            onChangeText={setFI}
             placeholder={'Enter Protein Goal'}
             placeholderTextColor={'rgba(255,255,255,0.6)'}
             style={Styles.InputStyles}
@@ -34,10 +61,10 @@ const CustomForm = (props) => {
           />
         </View>
         <View style={Styles.InputView1}>
-          <Text>{'Protein: '}</Text>
+          <Text style={Styles.InputText}>{'Protein: '}</Text>
           <TextInput
-            value={MacroInput}
-            onChangeText={setMacroI}
+            value={CI}
+            onChangeText={setCI}
             placeholder={'Enter Protein Goal'}
             placeholderTextColor={'rgba(255,255,255,0.6)'}
             style={Styles.InputStyles}
@@ -45,30 +72,53 @@ const CustomForm = (props) => {
           />
         </View>
         <View style={Styles.InputView1}>
-          <Text>{'Protein: '}</Text>
-          <TextInput
-            value={MacroInput}
-            onChangeText={setMacroI}
-            placeholder={'Enter Protein Goal'}
-            placeholderTextColor={'rgba(255,255,255,0.6)'}
-            style={Styles.InputStyles}
-            keyboardType={'decimal-pad'}
-          />
+          <View style={Styles.InputSubview}>
+            <Text style={Styles.InputText}>{'Protein'}</Text>
+            <View
+              style={{
+                alignSelf: 'center',
+                height: 17,
+                width: 2,
+                backgroundColor: Colors.grey,
+                marginRight: 10,
+                marginLeft: 10,
+              }}
+            />
+            <TextInput
+              value={SI}
+              onChangeText={setSI}
+              placeholder={'Goal'}
+              placeholderTextColor={'rgba(255,255,255,0.6)'}
+              style={Styles.InputStyles}
+              keyboardType={'decimal-pad'}
+              textAlign={'center'}
+            />
+            {SI.length > 0 ? (
+              <TouchableOpacity onPress={() => setSI('')}>
+                <Icon
+                  name={'remove'}
+                  size={17}
+                  color={'white'}
+                  style={{marginLeft: 5}}
+                />
+              </TouchableOpacity>
+            ) : null}
+          </View>
         </View>
         <View style={Styles.InputView1}>
-          <Text>{'Protein: '}</Text>
+          <Text style={Styles.InputText}>{'Protein: '}</Text>
           <TextInput
-            value={MacroInput}
-            onChangeText={setMacroI}
+            value={SOI}
+            onChangeText={setSOI}
             placeholder={'Enter Protein Goal'}
             placeholderTextColor={'rgba(255,255,255,0.6)'}
             style={Styles.InputStyles}
             keyboardType={'decimal-pad'}
           />
         </View>
-<TouchableOpacity style={{alignSelf: 'center'}}>
-  <Text>Change Goals</Text>
-</TouchableOpacity>
+        <TouchableOpacity style={{alignSelf: 'center'}}>
+          <Text>Change Goals</Text>
+        </TouchableOpacity>
       </View>
     </ScrollView>
   );
