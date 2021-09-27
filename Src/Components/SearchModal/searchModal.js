3a3ab97321c51eaa58d5c74881ea5999e5de4a44
edwardcordero <edwardcordero98@gmail.com>
2021-09-26 17:04:03 -0700

    Update search modal

--- Src/Components/SearchModal/searchModal.js
+++ Src/Components/SearchModal/searchModal.js
@@ -35,6 +35,7 @@ const SearchModal = (props) => {
     }});
     
     totalResults.current = responseObjects.data.foods;
+    console.log(totalResults.current)
     if(responseObjects.data.foods.food.length > 0){
       const results = responseObjects.data.foods.food.map(obj=>{
         return {name: obj.food_name, toggle: false,id: (Math.random() % 100), foodType: obj.food_type,food_description: obj.food_description, food_id: obj.food_id,
